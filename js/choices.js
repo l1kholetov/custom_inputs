31549895608ede91afdfeaf6fb9fbdd0fb33aff4
l1kholetov <117196655+l1kholetov@users.noreply.github.com>
2024-04-29 23:25:37 +0300

    Подключение SimpleBar

--- js/choices.js
+++ js/choices.js
@@ -1,5 +1,6 @@
 const element = document.querySelector('.select__js');
 const choices = new Choices(element, {
+  allowHTML: true,
   searchEnabled: false,
   placeholder: true,
   searchPlaceholderValue: 'Поиск материала',
---
+++ js/simplebar.js
@@ -0,0 +1,4 @@
+new SimpleBar(document.querySelector('.customScroll'), {
+  autoHide: false,
+  scrollbarMaxSize: 84
+});
