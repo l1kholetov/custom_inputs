869471e0908217d0cda0fcbbe6fafb13ce756008
l1kholetov <117196655+l1kholetov@users.noreply.github.com>
2024-05-03 18:25:35 +0300

    Добавление tooltip

--- js/map.js
+++ js/map.js
@@ -3,11 +3,11 @@
       ymaps.ready(function () {
 
         var myMap = new ymaps.Map("map", {
-          center: [59.939032, 30.315827],
-          zoom: 13
+          center: [48.872185, 2.354224],
+          zoom: 14
         });
       // Создание геообъекта с типом точка (метка).
-        var myPlacemark = new ymaps.Placemark([59.939032, 30.315827], {}, {
+        var myPlacemark = new ymaps.Placemark([48.872185, 2.354224], {}, {
           iconLayout: 'default#image',
           iconImageHref: '/img/svg/map.svg',
           iconImageSize: [48, 48],
